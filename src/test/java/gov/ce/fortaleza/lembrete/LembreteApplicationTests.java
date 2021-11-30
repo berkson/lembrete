875871184672221e16cc961b23265ca806f18f90
berkson <berksonx@yahoo.com.br>
2021-11-29 22:35:37 -0300

    first commit

---
+++ src/test/java/gov/ce/fortaleza/lembrete/LembreteApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.ce.fortaleza.lembrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LembreteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
