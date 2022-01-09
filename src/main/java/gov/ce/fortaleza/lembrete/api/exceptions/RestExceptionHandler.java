79f479efddb73f916ce5359d2d9b580bba0ce476
Berkson <berksonx@yahoo.com.br>
2022-01-08 23:24:20 -0300

    mais melhorias no manejo de erros do sistema.

--- src/main/java/gov/ce/fortaleza/lembrete/api/exceptions/RestExceptionHandler.java
+++ src/main/java/gov/ce/fortaleza/lembrete/api/exceptions/RestExceptionHandler.java
@@ -8,19 +8,23 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.EntityNotFoundException;
+import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Created by berkson
@@ -35,7 +39,6 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex, HttpHeaders headers,
             HttpStatus status, WebRequest request) {
-        log.info("Status code: " + status.value() + " description: " + status.getReasonPhrase());
         Map<String, Object> validationErrors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
@@ -52,29 +55,63 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
                 .error(apiError).build(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiErrors constraintViolationException(ConstraintViolationException ex, WebRequest request) {
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+            HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status,
+            WebRequest request) {
 
-        String message = ex.getMessage().substring(ex.getMessage().indexOf(" ") + 1);
+        String method = Optional.ofNullable(((ServletWebRequest) request).getHttpMethod())
+                .map(Enum::name).orElse("");
 
         ApiError apiError = new ApiError(LocalDateTime.now(),
-                HttpStatus.BAD_REQUEST, message,
+                status, "Método de requisição " + method + " não suportado",
+                ((ServletWebRequest) request).getRequest().getRequestURI());
+
+        return new ResponseEntity<>(ApiErrors.builder().error(apiError).build(),
+                HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex,
+                                                                   HttpHeaders headers,
+                                                                   HttpStatus status,
+                                                                   WebRequest request) {
+        ApiError apiError = new ApiError(LocalDateTime.now(),
+                status, "Nenhum serviço encontrado para requisição '" +
+                ex.getHttpMethod() + "' na url", ex.getRequestURL());
+
+        return new ResponseEntity<>(ApiErrors.builder().error(apiError).build(),
+                HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleConstraintViolation(ConstraintViolationException ex, WebRequest request) {
+        Map<String, Object> map = new HashMap<>();
+        int count = 1;
+
+        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+            String value = "Valor " + violation.getInvalidValue() + " " + violation.getMessage();
+            map.put("restriction_" + count, value);
+            count++;
+        }
+
+        ApiError apiError = new ValidationError(LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST, "Erro(s) de restrição", map,
                 ((ServletWebRequest) request).getRequest().getRequestURI());
 
         return ApiErrors.builder().error(apiError).build();
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<Object> entityNotFoundException(EntityNotFoundException ex,
-                                                          WebRequest request) {
+    public ResponseEntity<Object> handleEntityNotFound(WebRequest request) {
         ApiError apiError = new ApiError(LocalDateTime.now(),
                 HttpStatus.BAD_REQUEST, "Não encontrado",
                 ((ServletWebRequest) request).getRequest().getRequestURI());
 
-        return new ResponseEntity(ApiErrors.builder()
+        return new ResponseEntity<>(ApiErrors.builder()
                 .error(apiError).build(), HttpStatus.BAD_REQUEST);
     }
-
 
 }
